039851f323b6668f26cf35d946d5ace8b82de9cd
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2025-04-23 19:21:53 -0300

    tres

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.9581099682496!2d-58.50496522487762!3d-34.73145026425415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccef8ac7acf83%3A0x308e92157265a081!2sGolf%20Club%20Eventos!5e0!3m2!1ses-419!2sar!4v1719444128246!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2983.260436632888!2d-65.3586418!3d-41.6068796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95f91dbb95e94309%3A0x396ac2dc3abd24f9!2sPelotero%20y%20Sal%C3%B3n%20de%20eventos%20%22Sonrisitas%22!5e0!3m2!1ses-419!2sar!4v1745446882415!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
